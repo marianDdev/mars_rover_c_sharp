af09f11dd99305ae373d5be0c0e43da0980de049
Marian Dumitru <d.mariandev@gmail.com>
2022-07-08 19:31:08 +0300

    WIP - mars_rover change rover postition and orienatation from CLI

---
+++ mars_rover/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace mars_rover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
